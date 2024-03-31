3847babc98622be298138a2eb73bb5e9858757aa
chris byron <chris.byron@gmail.com>
2024-03-31 10:11:17 -0700

    Initial project setup

---
+++ src/main/java/com/jsonwebapp/demo/JsonWebappApplication.java
@@ -0,0 +1,13 @@
+package com.jsonwebapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsonwebapp/demo/JsonWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsonwebapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
